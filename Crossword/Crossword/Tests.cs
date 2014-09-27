09cdffb27d09d462c51373f14ea502154e492e41
samblackburn <sam.blackburn@red-gate.com>
2014-09-27 21:57:03 +0100

    An assertion about words

--- Crossword/Crossword/Tests.cs
+++ Crossword/Crossword/Tests.cs
@@ -21,5 +21,30 @@ namespace Crossword
             Assert.AreEqual(1, result[0]);
             Assert.False(result.Read());
         }
+         
+        [Test]
+        public void CanConnectToDictionaryDatabase()
+        {
+            var conn = new MySqlConnection("Data Source=localhost;Database=wn_pro_mysql;User ID=sam;Password=;Old Guids=true;");
+            conn.Open();
+            var result = new MySqlCommand("SELECT 1", conn).ExecuteReader();
+            Assert.True(result.Read());
+            Assert.AreEqual(1, result[0]);
+            Assert.False(result.Read());
+        }
+
+        [Test]
+        public void FoodlessMeansMalnourished()
+        {
+            var conn = new MySqlConnection("Data Source=localhost;Database=wn_pro_mysql;User ID=sam;Password=;Old Guids=true;");
+            conn.Open();
+            var result = new MySqlCommand("SELECT two.Word FROM wn_synset one, wn_synset two, wn_similar relation WHERE relation.synset_id_1 = one.synset_id AND relation.synset_id_2 = two.synset_id AND one.Word = 'malnourished'", conn).ExecuteReader();
+            var list = new List<string>();
+            while (result.Read())
+            {
+                list.Add(result["Word"] as string);
+            }
+            CollectionAssert.Contains(list, "foodless");
+        }
     }
 }
