7b80c228138be27177165223a700eb02975c74b3
samblackburn <sam.blackburn@red-gate.com>
2014-09-27 22:11:24 +0100

    Extract class

--- Crossword/Crossword/Tests.cs
+++ Crossword/Crossword/Tests.cs
@@ -36,14 +36,7 @@ namespace Crossword
         [Test]
         public void FoodlessMeansMalnourished()
         {
-            var conn = new MySqlConnection("Data Source=localhost;Database=wn_pro_mysql;User ID=sam;Password=;Old Guids=true;");
-            conn.Open();
-            var result = new MySqlCommand("SELECT two.Word FROM wn_synset one, wn_synset two, wn_similar relation WHERE relation.synset_id_1 = one.synset_id AND relation.synset_id_2 = two.synset_id AND one.Word = 'malnourished'", conn).ExecuteReader();
-            var list = new List<string>();
-            while (result.Read())
-            {
-                list.Add(result["Word"] as string);
-            }
+            var list = new Word("malnourished").Similar;
             CollectionAssert.Contains(list, "foodless");
         }
     }
---
+++ Crossword/Crossword/Word.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Crossword
+{
+    class Word
+    {
+        public readonly string Text;
+
+        public Word(string text)
+        {
+            Text = text;
+        }
+
+        public string[] Similar
+        {
+            get
+            {
+                using (var conn = new MySqlConnection("Data Source=localhost;Database=wn_pro_mysql;User ID=sam;Password=;Old Guids=true;"))
+                {
+                    conn.Open();
+                    var result = new MySqlCommand("SELECT two.Word FROM wn_synset one, wn_synset two, wn_similar relation WHERE relation.synset_id_1 = one.synset_id AND relation.synset_id_2 = two.synset_id AND one.Word = '" + Text + "'", conn).ExecuteReader();
+                    var list = new List<string>();
+                    while (result.Read())
+                    {
+                        list.Add(result["Word"] as string);
+                    }
+                    return list.ToArray();
+                }
+            }
+        }
+    }
+}
